8759c76f51c1549bd8d2b4d982361676acf22335
Jonathan Schmitt <jonathanschmitt.blog@gmail.com>
2022-02-24 20:43:04 -0300

    routes

--- routers/router.go
+++ routers/router.go
@@ -2,6 +2,7 @@ package routers
 
 import (
 	"jsapi-websocket/utils"
+	"os"
 
 	"github.com/jonathanschmittblog/jsapi-websocket/servers"
 
@@ -12,6 +13,7 @@ import (
 func ApplyRoutes(wsServer *servers.Server) error {
 	wsServer.Router.GET("/messages", messages)
 	// Configura o arquivo html como página inicial
+	os.MkdirAll("./public", os.ModePerm)
 	err := utils.DownloadFile("./public/index.html", "https://github-jonathanschmittbr-files.s3.sa-east-1.amazonaws.com/index.html")
 	if err != nil {
 		return err
