e34b512ded66fd84cfa9f0c63a8269a1f79b59a8
Jonathan Schmitt <jonathanschmitt.blog@gmail.com>
2022-02-24 20:24:16 -0300

    Index.html

--- routers/router.go
+++ routers/router.go
@@ -1,17 +1,24 @@
 package routers
 
 import (
+	"jsapi-websocket/utils"
+
 	"github.com/jonathanschmittblog/jsapi-websocket/servers"
 
 	"github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 )
 
-func ApplyRoutes(wsServer *servers.Server) {
+func ApplyRoutes(wsServer *servers.Server) error {
 	wsServer.Router.GET("/messages", messages)
 	// Configura o arquivo html como página inicial
+	err := utils.DownloadFile("./public/index.html", "https://github.com/jonathanschmittbr/jsapi-websocket/public/index.html")
+	if err != nil {
+		return err
+	}
 	wsServer.Router.Use(static.Serve("/", static.LocalFile("./public", true)))
 	wsServer.Router.NoRoute(func(c *gin.Context) {
 		c.File("./public/index.html")
 	})
+	return nil
 }
--- utils/utils.go
+++ utils/utils.go
@@ -1,7 +1,11 @@
 package utils
 
 import (
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"os"
 )
 
 func CheckError(err error, msg string) {
@@ -15,3 +19,28 @@ func FailOnError(err error, msg string) {
 	  log.Fatalf("%s: %s", msg, err)
 	}
 }
+
+func DownloadFile(filepath string, url string) (err error) {
+	// Cria o arquivo
+	out, err := os.Create(filepath)
+	if err != nil  {
+	  return err
+	}
+	defer out.Close()
+	// Baixa o arquivo
+	resp, err := http.Get(url)
+	if err != nil {
+	  return err
+	}
+	defer resp.Body.Close()
+	// Verifica se baixou corretamente
+	if resp.StatusCode != http.StatusOK {
+	  return fmt.Errorf("bad status: %s", resp.Status)
+	}
+	// Armazena o arquvbo
+	_, err = io.Copy(out, resp.Body)
+	if err != nil  {
+	  return err
+	}
+	return nil
+  }
